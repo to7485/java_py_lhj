374afc72c4e6da25cf19ba823d3451d681544c0f
to7485 <54658614+to7485@users.noreply.github.com>
2025-11-28 10:46:44 +0900

    11-28

---
+++ 3.java/work/ex_1128/src/lombok/Member.java
@@ -0,0 +1,20 @@
+package lombok;
+
+public class Member {
+
+	private Long id;
+	private String name;
+	private String email;
+	
+	//기본생성자
+	
+	//필드를 초기화 하는 생성자
+	
+	//getter & setter
+	
+	
+	
+	
+	
+	
+}
